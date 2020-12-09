b0e0e0b50b1c9f429a3699249da172e9daaad1ea
ElectricPotato <mironandrzej1@gmail.com>
2020-12-09 12:13:25 +0000

    day9

--- 2020/day09/day9.py
+++ 2020/day09/day9.py
@@ -3,13 +3,24 @@
 '''
 
 def partA(inputText):
-    
+    numbers = [int(n) for n in inputText.split()]
+    preamble = 25
+    for i in range(preamble,len(numbers)):
+        if(not sum([numbers[i]-j in numbers[i-preamble:i] and numbers[i] != j for j in numbers[i-preamble:i]])):
+            return numbers[i]
+
 
 def partB(inputText):
-    return None
+    numbers = [int(n) for n in inputText.split()]
+    goalSum = partA(inputText)
+    for starti in range(0,len(numbers)-1):
+        for endi in range(starti+2,len(numbers)+1):
+            if(sum(numbers[starti:endi]) == goalSum):
+                return min(numbers[starti:endi])+max(numbers[starti:endi])
 
 
 
+#inputFile = open("inputExample.txt", "r")
 inputFile = open("input.txt", "r")
 inputText = inputFile.read()
 
