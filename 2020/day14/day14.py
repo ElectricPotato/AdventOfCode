b6379f1bea546c03215938f50d182337f571049d
ElectricPotato <mironandrzej1@gmail.com>
2020-12-17 00:44:16 +0000

    day14

--- 2020/day14/day14.py
+++ 2020/day14/day14.py
@@ -3,14 +3,55 @@
 '''
 
 def partA(inputText):
-    
+    lines = inputText.splitlines()
+    andMask = 0
+    orMask = 0
+    mem = {}
+    for line in lines:
+        destination, literal = line.split(" = ")
+        if(destination=="mask"):
+            andMask = int(literal.replace("X","1"),2)
+            orMask  = int(literal.replace("X","0"),2)
+            #print(orMask,andMask)
+        elif(destination[:4] == "mem["):
+            address = int(destination[4:-1])
+            translatedN = (int(literal) & andMask) | orMask
+            mem[address] = translatedN
+            #print(mem)
+
+    return sum(mem.values())
 
 def partB(inputText):
-    return None
+    lines = inputText.splitlines()
+    orMask = 0
+    mem = {}
+    for line in lines:
+        destination, literal = line.split(" = ")
+        if(destination=="mask"):
+            xIndices = [pos for pos,char in enumerate(literal) if char=="X"]
+            noAddresses = 2**len(xIndices)
+            orMask = int(literal.replace("X","0"),2)
+        elif(destination[:4] == "mem["):
+            address = f'{(int(destination[4:-1]) | orMask):0>36b}'
+            translatedN = int(literal)
+            #print( f'{orMask:0>36b}')
+            for n in range(noAddresses):
+                nBin = f'{n:0>36b}'
+                newAddress = list(address)
+                for i in range(len(xIndices)):
+                    newAddress[xIndices[i]] = nBin[-i-1]
+                newAddress=int(''.join(newAddress),2)
+                mem[newAddress] = translatedN
+                #print(newAddress)
+            #print(mem)
+
+    return sum(mem.values())
 
 
 
 inputFile = open("input.txt", "r")
+#inputFile = open("inputEx.txt", "r")
+#inputFile = open("inputEx2.txt", "r")
 inputText = inputFile.read()
 
 print("partA", partA(inputText))
