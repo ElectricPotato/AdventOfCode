3ff7acfee406771c096c0ac04dbcbd2d28a8e134
ElectricPotato <mironandrzej1@gmail.com>
2022-12-09 05:31:15 +0000

    add day 9

--- 2022/day09/day09.py
+++ 2022/day09/day09.py
@@ -2,20 +2,60 @@
 
 '''
 
+def sgn(x):
+    if(x>0):return 1
+    if(x<0):return -1
+    return 0
 
 def partA(inputText):
     lines=inputText.split("\n")
+    x, y = 0, 0
+    tailx, taily = 0, 0
+    allTailPositions = [(tailx, taily)]
     for line in lines:
-        pass
+        dir, n = line.split()
+        n=int(n)
+        xDir, yDir = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}[dir]
+        for i in range(n):
+            x += xDir
+            y += yDir
 
-    return 1
+            #update tail pos
+            if( abs(x - tailx) > 1 or abs(y - taily) > 1 ):
+                    tailx += sgn(x - tailx)
+                    taily += sgn(y - taily)
+
+            if((tailx, taily) not in allTailPositions):
+                allTailPositions += [(tailx, taily)]
+
+            #print( (x,y), (tailx, taily))
+
+    return len(allTailPositions)
 
 def partB(inputText):
     lines=inputText.split("\n")
+    rope = [[0,0] for i in range(10)]
+    allTailPositions = [[rope[-1][0], rope[-1][1]]]
     for line in lines:
-        pass
+        dir, n = line.split()
+        n=int(n)
+        xDir, yDir = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}[dir]
+        for i in range(n):
+            rope[0][0] += xDir
+            rope[0][1] += yDir
 
-    return 1
+            for i in range(1, len(rope)):
+                if( abs(rope[i-1][0] - rope[i][0]) > 1 or abs(rope[i-1][1] - rope[i][1]) > 1 ):
+                    rope[i][0] += sgn(rope[i-1][0] - rope[i][0])
+                    rope[i][1] += sgn(rope[i-1][1] - rope[i][1])
+
+            if([rope[-1][0], rope[-1][1]] not in allTailPositions):
+                allTailPositions += [[rope[-1][0], rope[-1][1]]]
+
+            #print( (x,y), (tailx, taily), allTailPositions)
+            #print(rope)
+
+    return len(allTailPositions)
 
 
 
@@ -33,5 +73,5 @@ print("Example partA", partA(inputText))
 print("Example partB", partB(inputText))
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
-#print("partB", partB(inputText))
+print("partA", partA(inputText))
+print("partB", partB(inputText))
