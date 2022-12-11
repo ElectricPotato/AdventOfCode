37f3a7f9716d92e1b9b66095882b79c69e051609
ElectricPotato <mironandrzej1@gmail.com>
2022-12-11 06:04:43 +0000

    clean up division test

--- 2022/day11/day11.py
+++ 2022/day11/day11.py
@@ -1,8 +1,8 @@
 class Monkey:
-    def __init__(self, startItems, operation, testOp, ifTrueDest, ifFalseDest) -> None:
+    def __init__(self, startItems, operation, divisionTest, ifTrueDest, ifFalseDest) -> None:
         self.items = startItems
         self.operation = operation
-        self.testOp = testOp
+        self.divisionTest = divisionTest
         self.ifTrueDest = ifTrueDest
         self.ifFalseDest = ifFalseDest
         self.inspectN = 0
@@ -11,28 +11,28 @@ monkeysExample = [
     Monkey(
         [79, 98],
         lambda old: old * 19,
-        lambda x: x % 23 == 0,
+        23,
         2,
         3
     ),
     Monkey(
         [54, 65, 75, 74],
         lambda old: old + 6,
-        lambda x: x % 19 == 0,
+        19,
         2,
         0
     ),
     Monkey(
         [79, 60, 97],
         lambda old: old * old,
-        lambda x: x % 13 == 0,
+        13,
         1,
         3
     ),
     Monkey(
         [74],
         lambda old: old + 3,
-        lambda x: x % 17 == 0,
+        17,
         0,
         1
     )
@@ -42,56 +42,56 @@ monkeysProblem = [
   Monkey(
       [98, 89, 52],
       lambda old: old * 2,
-      lambda x: x % 5 == 0,
+      5,
       6,
       1
   ),
   Monkey(
       [57, 95, 80, 92, 57, 78],
       lambda old: old * 13,
-      lambda x: x % 2 == 0,
+      2,
       2,
       6
   ),
   Monkey(
       [82, 74, 97, 75, 51, 92, 83],
       lambda old: old + 5,
-      lambda x: x % 19 == 0,
+      19,
       7,
       5
   ),
   Monkey(
       [97, 88, 51, 68, 76],
       lambda old: old + 6,
-      lambda x: x % 7 == 0,
+      7,
       0,
       4
   ),
   Monkey(
       [63],
       lambda old: old + 1,
-      lambda x: x % 17 == 0,
+      17,
       0,
       1
   ),
   Monkey(
       [94, 91, 51, 63],
       lambda old: old + 4,
-      lambda x: x % 13 == 0,
+      13,
       4,
       3
   ),
   Monkey(
       [61, 54, 94, 71, 74, 68, 98, 83],
       lambda old: old + 2,
-      lambda x: x % 3 == 0,
+      3,
       2,
       7
   ),
   Monkey(
       [90, 56],
       lambda old: old * old,
-      lambda x: x % 11 == 0,
+      11,
       3,
       5
   )
@@ -106,20 +106,29 @@ def partA(monkeys: List[Monkey]):
                 monkey.items[0] = monkey.operation(monkey.items[0])
                 monkey.inspectN += 1
                 monkey.items[0] = monkey.items[0] // 3
-                destination = [monkey.ifFalseDest, monkey.ifTrueDest][monkey.testOp(monkey.items[0])]
+                test = monkey.items[0] % monkey.divisionTest == 0
+                destination = [monkey.ifFalseDest, monkey.ifTrueDest][test]
                 monkeys[destination].items += [monkey.items.pop(0)]
 
     inspectStats = sorted(monkey.inspectN for monkey in monkeys)
 
     return inspectStats[-1] * inspectStats[-2]
 
-def partB(monkeys, modulo):
+
+from functools import reduce
+def product(xs):
+    return reduce(lambda x, y: x * y, xs, 1)
+
+def partB(monkeys):
+    modulo = product(monkey.divisionTest for monkey in monkeys)
+
     for roundN in range(10000):
         for monkey in monkeys:
             while(len(monkey.items) > 0):
                 monkey.items[0] = monkey.operation(monkey.items[0]) % modulo
                 monkey.inspectN += 1
-                destination = [monkey.ifFalseDest, monkey.ifTrueDest][monkey.testOp(monkey.items[0])]
+                test = monkey.items[0] % monkey.divisionTest == 0
+                destination = [monkey.ifFalseDest, monkey.ifTrueDest][test]
                 monkeys[destination].items += [monkey.items.pop(0)]
 
     inspectStats = sorted(monkey.inspectN for monkey in monkeys)
@@ -137,8 +146,10 @@ def readFile(fileName):
         inputText = inputFile.read()
     return inputText
 
-#print("Example partA", partA(monkeysExample))
-print("Example partB", partB(monkeysExample, 23*19*13*17))
+from copy import deepcopy
 
-#print("partA", partA(monkeysProblem))
-print("partB", partB(monkeysProblem, 5*2*19*7*17*13*3*11))
+print("Example partA", partA(deepcopy(monkeysExample)))
+print("Example partB", partB(deepcopy(monkeysExample)))
+
+print("partA", partA(deepcopy(monkeysProblem)))
+print("partB", partB(deepcopy(monkeysProblem)))
