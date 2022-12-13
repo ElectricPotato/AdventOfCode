461477e7610db13bfafcd679e5d8d37450ac2f33
ElectricPotato <mironandrzej1@gmail.com>
2022-12-13 05:39:47 +0000

    day 13

--- 2022/day13/day13.py
+++ 2022/day13/day13.py
@@ -4,18 +4,77 @@
 
 
 def partA(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
 
-    return 1
+    def compare(a, b, debug=True):
+        #if(debug and not ((type(a) == int and type(b) == int))): print(a,b, compare(a, b, False))
+
+        if(type(a) == int and type(b) == int):
+            if(a != b):
+                return a < b
+        elif(type(a) == list and type(b) == list):
+            for x, y in zip(a,b):
+                if(compare(x,y) is not None):
+                    return compare(x,y)
+            if(len(a) > len(b)):
+                return False
+            if(len(a) < len(b)):
+                return True
+        elif(type(a) == int and type(b) == list):
+            return compare([a], b)
+        elif(type(a) == list and type(b) == int):
+            return compare(a, [b])
+
+        return None
+
+    total = 0
+    pairs = inputText.split("\n\n")
+    for i, pair in enumerate(pairs, start = 1):
+        a, b = pair.split("\n")
+        a = eval(a)
+        b = eval(b)
+        #print(a,b,compare(a,b))
+        if(compare(a, b, True)):
+            total += i
+
+    return total
 
 def partB(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    def compare(a, b, debug=True):
+        if(type(a) == int and type(b) == int):
+            if(a != b):
+                return a < b
+        elif(type(a) == list and type(b) == list):
+            for x, y in zip(a,b):
+                if(compare(x,y) is not None):
+                    return compare(x,y)
+            if(len(a) > len(b)):
+                return False
+            if(len(a) < len(b)):
+                return True
+        elif(type(a) == int and type(b) == list):
+            return compare([a], b)
+        elif(type(a) == list and type(b) == int):
+            return compare(a, [b])
 
-    return 1
+        return None
+
+
+    packets = [eval(packet) for packet in inputText.split("\n") if packet != ""]
+
+    packets += [ [[2]], [[6]] ]
+
+    #bubble sort
+    isSorted = False
+    while(not isSorted):
+        isSorted = True
+        for i in range(len(packets)-1):
+            if(compare(packets[i], packets[i+1]) == False):
+                packets[i], packets[i+1] = packets[i+1], packets[i] #swap
+                isSorted = False
+
+    #for p in packets:print(p)
+
+    return (packets.index([[2]])+1) * (packets.index([[6]])+1)
 
 
 
@@ -33,5 +92,5 @@ print("Example partA", partA(inputText))
 print("Example partB", partB(inputText))
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
-#print("partB", partB(inputText))
+print("partA", partA(inputText))
+print("partB", partB(inputText))
