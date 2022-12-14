3c4e15d7342394c4f92abbbf9cb1bec556a8c4c7
ElectricPotato <mironandrzej1@gmail.com>
2022-12-14 06:14:34 +0000

    day14

--- 2022/day14/day14.py
+++ 2022/day14/day14.py
@@ -2,20 +2,128 @@
 
 '''
 
+def sgn(x):
+    if(x>0):return 1
+    if(x<0):return -1
+    return 0
 
 def partA(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    points = []
+    paths=inputText.split("\n")
+    maxY = 0
+    for path in paths:
+        pathParsed = []
+        for line in path.split(" -> "):
+            x,y = line.split(",")
+            x,y = int(x), int(y)
+            pathParsed += [[x,y]]
 
-    return 1
+            maxY = max(maxY, y)
+
+        for pointA, pointB in zip(pathParsed, pathParsed[1:]):
+            xA, yA = pointA
+            xB, yB = pointB
+
+            xDir, yDir = sgn(xB - xA), sgn(yB - yA)
+
+            xCurr, yCurr = xA, yA
+            points += [[xCurr, yCurr]]
+
+            while(not(xCurr == xB and yCurr == yB)):
+                xCurr, yCurr = xCurr + xDir, yCurr + yDir
+                points += [[xCurr, yCurr]]
+
+    sandGrains = 0
+    while(True):
+        xSand, ySand = 500, 0
+        moved = True
+        while(moved):
+            moved = False
+            for xDir, yDir in [[0,1], [-1,1], [1,1]]:
+                if([xSand + xDir, ySand + yDir] not in points):
+                    moved = True
+                    xSand, ySand = xSand + xDir, ySand + yDir
+                    break
+            
+            if(ySand > maxY):
+                break
+        
+        if(moved == False):
+            points += [[xSand, ySand]]
+            sandGrains += 1
+        else:
+            break
+    return sandGrains
+
+def printGrid(points, xSand, ySand):
+    xMax = max(p[0] for p in points)
+    xMin = min(p[0] for p in points)
+
+    yMax = max(p[1] for p in points)
+    yMin = min(p[1] for p in points)
+
+    for y in range(yMin, yMax+1):
+        line = ""
+        for x in range(xMin, xMax+1):
+            if([x,y] == [xSand, ySand]):
+                line += "s"
+            elif([x,y] in points):
+                line += "#"
+            else:
+                line += "."
+        print(line)
 
 def partB(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    points = []
+    paths=inputText.split("\n")
+    maxY = 0
+    for path in paths:
+        pathParsed = []
+        for line in path.split(" -> "):
+            x,y = line.split(",")
+            x,y = int(x), int(y)
+            pathParsed += [[x,y]]
 
-    return 1
+            maxY = max(maxY, y)
+
+        for pointA, pointB in zip(pathParsed, pathParsed[1:]):
+            xA, yA = pointA
+            xB, yB = pointB
+
+            xDir, yDir = sgn(xB - xA), sgn(yB - yA)
+
+            xCurr, yCurr = xA, yA
+            points += [[xCurr, yCurr]]
+
+            while(not(xCurr == xB and yCurr == yB)):
+                xCurr, yCurr = xCurr + xDir, yCurr + yDir
+                points += [[xCurr, yCurr]]
+
+    sandGrains = 0
+    while(True):
+        xSand, ySand = 500, 0
+        while(True):
+            moved = False
+            for xDir, yDir in [[0,1], [-1,1], [1,1]]:
+                if([xSand + xDir, ySand + yDir] not in points):
+                    moved = True
+                    xSand, ySand = xSand + xDir, ySand + yDir
+                    break
+
+            if(not moved): break
+            if(ySand == maxY + 1): break
+        
+        points += [[xSand, ySand]]
+        sandGrains += 1
+        if(sandGrains%100 == 0):print(sandGrains)
+
+        
+        if([xSand, ySand] == [500,0]):
+            break
+
+    #printGrid(points, xSand, ySand)
+
+    return sandGrains
 
 
 
@@ -35,3 +143,4 @@ print("Example partB", partB(inputText))
 inputText = readFile("input.txt")
 #print("partA", partA(inputText))
 #print("partB", partB(inputText))
+
