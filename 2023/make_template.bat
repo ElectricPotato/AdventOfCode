3ba009b852c1f6535f44e8695e78673dfade9d87
ElectricPotato <mironandrzej1@gmail.com>
2023-12-05 20:32:49 +0000

    add 2023 template

---
+++ 2023/make_template.bat
@@ -0,0 +1,52 @@
+mkdir day01
+mkdir day02
+mkdir day03
+mkdir day04
+mkdir day05
+mkdir day06
+mkdir day07
+mkdir day08
+mkdir day09
+mkdir day10
+mkdir day11
+mkdir day12
+mkdir day13
+mkdir day14
+mkdir day15
+mkdir day16
+mkdir day17
+mkdir day18
+mkdir day19
+mkdir day20
+mkdir day21
+mkdir day22
+mkdir day23
+mkdir day24
+mkdir day25
+
+
+cp .\template.py day01\day01.py
+cp .\template.py day02\day02.py
+cp .\template.py day03\day03.py
+cp .\template.py day04\day04.py
+cp .\template.py day05\day05.py
+cp .\template.py day06\day06.py
+cp .\template.py day07\day07.py
+cp .\template.py day08\day08.py
+cp .\template.py day09\day09.py
+cp .\template.py day10\day10.py
+cp .\template.py day11\day11.py
+cp .\template.py day12\day12.py
+cp .\template.py day13\day13.py
+cp .\template.py day14\day14.py
+cp .\template.py day15\day15.py
+cp .\template.py day16\day16.py
+cp .\template.py day17\day17.py
+cp .\template.py day18\day18.py
+cp .\template.py day19\day19.py
+cp .\template.py day20\day20.py
+cp .\template.py day21\day21.py
+cp .\template.py day22\day22.py
+cp .\template.py day23\day23.py
+cp .\template.py day24\day24.py
+cp .\template.py day25\day25.py
