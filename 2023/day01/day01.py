a1fc864ea045e3909f32855cb5d88e1495573e00
ElectricPotato <mironandrzej1@gmail.com>
2023-12-05 22:47:13 +0000

    solution day1

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -5,17 +5,49 @@
 
 def partA(inputText):
     lines=inputText.split("\n")
-    for line in lines:
-        pass
 
-    return 1
+    total = 0
+    for line in lines:
+        first_digit = None
+        last_digit = None
+        for c in line:
+            if(c.isdigit()):
+                if(first_digit == None): first_digit = int(c)
+                last_digit = int(c)
+        total += first_digit * 10 + last_digit
+        
+
+    return total
+
+import re
 
 def partB(inputText):
     lines=inputText.split("\n")
-    for line in lines:
-        pass
 
-    return 1
+    total = 0
+
+    numbers = "one|two|three|four|five|six|seven|eight|nine".split("|")
+    numbers_reversed = [n[::-1] for n in numbers]
+
+    pattern = r"(one|two|three|four|five|six|seven|eight|nine|\d)"
+    pattern_rev = "(" + "|".join(numbers_reversed) + r"|\d)"
+
+    def digit_to_int(s):
+        if(s.isdigit()):
+            return int(s)
+        else:
+            return numbers.index(s) + 1
+
+    for line in lines:
+        first = re.search(pattern, line)[0]
+        last = re.search(pattern_rev, line[::-1])[0][::-1]
+
+        first_digit = digit_to_int(first)
+        last_digit = digit_to_int(last)
+        
+        total += first_digit * 10 + last_digit
+
+    return total
 
 
 
@@ -30,8 +62,9 @@ def readFile(fileName):
 
 inputText = readFile("einput.txt")
 print("Example partA", partA(inputText))
+inputText = readFile("einput2.txt")
 print("Example partB", partB(inputText))
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
-#print("partB", partB(inputText))
+print("partA", partA(inputText))
+print("partB", partB(inputText))
