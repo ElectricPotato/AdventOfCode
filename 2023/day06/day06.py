794caaf65b0b7b0f416d350af3596d22fe517ab9
ElectricPotato <mironandrzej1@gmail.com>
2023-12-07 01:12:14 +0000

    add day 6

--- 2023/day06/day06.py
+++ 2023/day06/day06.py
@@ -2,20 +2,51 @@
 
 '''
 
+def parse(inputText):
+    timeStr, distStr = inputText.split("\n")
+    return list(zip(
+        map(int,timeStr.split()[1:]),
+        map(int,distStr.split()[1:])
+        ))
 
 def partA(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    races = parse(inputText)
 
-    return 1
+    product = 1
+    for time, dist in races:
+        product *= sum((time - buttonTime) * buttonTime > dist for buttonTime in range(time + 1))
 
+    return product
+
+def parseB(inputText):
+    timeStr, distStr = inputText.split("\n")
+    time = int(timeStr.split(":")[1].replace(" ", ""))
+    dist = int(distStr.split(":")[1].replace(" ", ""))
+    return (time, dist)
+
+
+'''
+t = total time, x = button time, d = distance
+(t - x)x = d
+tx - x^2 = d
+tx - x^2 - d = 0
+-x^2 + tx - d = 0
+
+quadratic formula
+format: ax^2 + bx + c = 0
+        a = -1, b = t, c = -d
+x = (-b +- sqrt(b^2 - 4ac)) / (2a)
+x = (-t +- sqrt(t^2 - 4(-1)(-d))) / (2(-1))
+x = (t +- sqrt(t^2 - 4d)) / 2
+'''
+
+import math
 def partB(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    time, dist = parseB(inputText)
+    buttonTime1 = (time - (time ** 2 - 4 * dist) ** 0.5) / 2
+    buttonTime2 = (time + (time ** 2 - 4 * dist) ** 0.5) / 2
 
-    return 1
+    return math.floor(buttonTime2) - math.ceil(buttonTime1) + 1
 
 
 
@@ -33,5 +64,5 @@ print("Example partA", partA(inputText))
 print("Example partB", partB(inputText))
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
-#print("partB", partB(inputText))
+print("partA", partA(inputText))
+print("partB", partB(inputText))
