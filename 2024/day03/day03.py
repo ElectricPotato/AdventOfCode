07984f12a40a9398f13661f9be1e68d1de0b1612
ElectricPotato <mironandrzej1@gmail.com>
2024-12-01 22:11:46 +0000

    create 2024 python templates

---
+++ 2024/day03/day03.py
@@ -0,0 +1,38 @@
+'''
+
+'''
+
+
+def partA(inputText):
+    lines=inputText.split("\n")
+    for line in lines:
+        pass
+
+    return 1
+
+def partB(inputText):
+    lines=inputText.split("\n")
+    for line in lines:
+        pass
+
+    return 1
+
+
+
+import os
+
+def readFile(fileName):
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__))
+    )
+    with open(os.path.join(__location__, fileName), "r") as inputFile:
+        inputText = inputFile.read()
+    return inputText
+
+inputText = readFile("einput.txt")
+print("Example partA", partA(inputText))
+print("Example partB", partB(inputText))
+
+inputText = readFile("input.txt")
+#print("partA", partA(inputText))
+#print("partB", partB(inputText))
---
+++ 2024/make_template.bat
@@ -0,0 +1,52 @@
+mkdir day01
+mkdir day02
+mkdir day03
+mkdir day04
+mkdir day05
+mkdir day06
+mkdir day07
+mkdir day08
+mkdir day09
+mkdir day10
+mkdir day11
+mkdir day12
+mkdir day13
+mkdir day14
+mkdir day15
+mkdir day16
+mkdir day17
+mkdir day18
+mkdir day19
+mkdir day20
+mkdir day21
+mkdir day22
+mkdir day23
+mkdir day24
+mkdir day25
+
+
+copy .\template.py day01\day01.py
+copy .\template.py day02\day02.py
+copy .\template.py day03\day03.py
+copy .\template.py day04\day04.py
+copy .\template.py day05\day05.py
+copy .\template.py day06\day06.py
+copy .\template.py day07\day07.py
+copy .\template.py day08\day08.py
+copy .\template.py day09\day09.py
+copy .\template.py day10\day10.py
+copy .\template.py day11\day11.py
+copy .\template.py day12\day12.py
+copy .\template.py day13\day13.py
+copy .\template.py day14\day14.py
+copy .\template.py day15\day15.py
+copy .\template.py day16\day16.py
+copy .\template.py day17\day17.py
+copy .\template.py day18\day18.py
+copy .\template.py day19\day19.py
+copy .\template.py day20\day20.py
+copy .\template.py day21\day21.py
+copy .\template.py day22\day22.py
+copy .\template.py day23\day23.py
+copy .\template.py day24\day24.py
+copy .\template.py day25\day25.py
