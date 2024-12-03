3dbdc43880a38a3e6c1014595acb28fd2862f25c
Miron Zadora <mironandrzej1@gmail.com>
2024-12-03 23:32:02 +0000

    day 2

--- 2024/day02/day02.py
+++ 2024/day02/day02.py
@@ -5,17 +5,67 @@
 
 def partA(inputText):
     lines=inputText.split("\n")
-    for line in lines:
-        pass
 
-    return 1
+    total = 0
+    for line in lines:
+        parsed = list(map(int, line.split()))
+        if parsed == []:
+            continue
+        
+        
+        safe = True
+        increasing = parsed[0] < parsed[1]
+        for a, b in zip(parsed, parsed[1:]):
+            if (increasing and (a > b)) \
+                or (not increasing and (a < b)) \
+                or not (1 <= abs(a - b) <= 3): #difference at least 1, at most 3
+                safe = False
+                break
+        
+        if safe:
+            total += 1
+
+    return total
+
+def findFault(parsed):
+    increasing = parsed[0] < parsed[1]
+    for i, (a, b) in enumerate(zip(parsed, parsed[1:])):
+        if (increasing and (a > b)) \
+            or (not increasing and (a < b)) \
+            or not (1 <= abs(a - b) <= 3): #difference at least 1, at most 3
+            return i
+    return -1
 
 def partB(inputText):
     lines=inputText.split("\n")
-    for line in lines:
-        pass
 
-    return 1
+    total = 0
+    for line in lines:
+        parsed = list(map(int, line.split()))
+        if parsed == []:
+            continue
+        
+        safe = True
+        fault = findFault(parsed)
+        if fault == -1:
+            safe = True
+        else:
+            safe = False
+            for i in range(-1, 2):
+                if (fault + i) >= len(parsed): continue
+                if (fault + i) < 0: continue
+                newParsed = list(parsed)
+                newParsed.pop(fault + i)
+                if findFault(newParsed) == -1:
+                    safe = True
+                    break
+            
+
+        
+        if safe:
+            total += 1
+
+    return total
 
 
 
@@ -30,9 +80,9 @@ def readFile(fileName):
     return inputText
 
 inputText = readFile("einput.txt")
-print("Example partA", partA(inputText))
-print("Example partB", partB(inputText))
+print("Example partA", partA(inputText)) #2
+print("Example partB", partB(inputText)) #4
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
-#print("partB", partB(inputText))
+print("partA", partA(inputText)) #407
+print("partB", partB(inputText)) #459
