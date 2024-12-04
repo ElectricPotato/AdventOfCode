4cb257df6861023e691e7d1caaa2b3bd9546362b
Miron Zadora <mironandrzej1@gmail.com>
2024-12-04 00:04:16 +0000

    day 3

--- 2024/day03/day03.py
+++ 2024/day03/day03.py
@@ -1,21 +1,48 @@
 '''
 
 '''
-
+import re
 
 def partA(inputText):
     lines=inputText.split("\n")
-    for line in lines:
-        pass
 
-    return 1
+    pattern = re.compile(r'mul\((\d*),(\d*)\)')
+    total = 0
+    for line in lines:
+        matches = pattern.finditer(line)
+        for match in matches:
+            n1, n2 = map(int, match.groups())
+            total += n1 * n2
+
+    return total
 
 def partB(inputText):
     lines=inputText.split("\n")
-    for line in lines:
-        pass
 
-    return 1
+    instrPatterns = [
+        r'do\(\)',
+        r'mul\((\d*),(\d*)\)',
+        r'don\'t\(\)'
+    ]
+
+    pattern = re.compile('|'.join(instrPatterns))
+    total = 0
+
+    enabled = True
+    for line in lines:
+        matches = pattern.finditer(line)
+        for match in matches:
+            print(match.group())
+            if match.group() == "do()":
+                enabled = True
+            elif match.group() == "don't()":
+                enabled = False
+            elif match.group()[:3] == "mul":
+                if enabled:
+                    n1, n2 = map(int, match.groups())
+                    total += n1 * n2
+
+    return total
 
 
 
@@ -31,8 +58,9 @@ def readFile(fileName):
 
 inputText = readFile("einput.txt")
 print("Example partA", partA(inputText))
+inputText = readFile("einputB.txt")
 print("Example partB", partB(inputText))
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
-#print("partB", partB(inputText))
+print("partA", partA(inputText))
+print("partB", partB(inputText))
