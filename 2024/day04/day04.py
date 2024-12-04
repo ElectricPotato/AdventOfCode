4c069c88090566108eb653ca4913e83131d8e907
Miron Zadora <mironandrzej1@gmail.com>
2024-12-04 21:16:41 +0000

    day 4 part A

--- 2024/day04/day04.py
+++ 2024/day04/day04.py
@@ -2,13 +2,39 @@
 
 '''
 
+dirs = [
+    ( 1,  0),
+    ( 1,  1),
+    ( 0,  1),
+    (-1,  1),
+    (-1,  0),
+    (-1, -1),
+    ( 0, -1),
+    ( 1, -1)
+]
 
 def partA(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    lines=inputText.split("\n")[:-1]
 
-    return 1
+    total = 0
+    #for each starting poisition
+    for y in range(len(lines)):
+        for x in range(len(lines[0])):
+            #search in all 8 directions
+            for dx, dy in dirs:
+                found = True
+                for i, c in enumerate("XMAS"):
+                    cx, cy = y + dy * i, x + dx * i
+                    if not ((0 <= cy < len(lines)) and \
+                            (0 <= cx < len(lines[0])) and \
+                            (lines[cy][cx] == c)):
+                        found = False
+                        break
+
+                if found:
+                    total += 1
+
+    return total
 
 def partB(inputText):
     lines=inputText.split("\n")
@@ -34,5 +60,5 @@ print("Example partA", partA(inputText))
 print("Example partB", partB(inputText))
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
-#print("partB", partB(inputText))
+print("partA", partA(inputText))
+print("partB", partB(inputText))
