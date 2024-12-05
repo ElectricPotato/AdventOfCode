52a01e229cd3a3acf6f27c817c5d47f943bfbd1f
Miron Zadora <mironandrzej1@gmail.com>
2024-12-05 14:25:08 +0000

    day 5 part A

--- 2024/day05/day05.py
+++ 2024/day05/day05.py
@@ -2,20 +2,48 @@
 
 '''
 
+def parse(inputText):
+    orderingRulesText, pageNoListsText = inputText.rstrip().split("\n\n")
+
+    orderingRules = [list(map(int, ruleText.split("|"))) for ruleText in orderingRulesText.split("\n")]
+    pageNoLists = [list(map(int, listText.split(","))) for listText in pageNoListsText.split("\n")]
+
+    return (orderingRules, pageNoLists)
+
+def isOrdered(pageNoList, orderingRules):
+    for pageA, pageB in orderingRules:
+        if pageA in pageNoList and pageB in pageNoList:
+            if pageNoList.index(pageA) > pageNoList.index(pageB):
+                return False
+    return True
+
+def fixOrdering(pageNoList, orderingRules): #TODO
+
+    return pageNoList
 
 def partA(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    orderingRules, pageNoLists = parse(inputText)
 
-    return 1
+    total = 0
+    for pageNoList in pageNoLists:
+        if isOrdered(pageNoList, orderingRules):
+            #add middle page
+            total += pageNoList[len(pageNoList) // 2]
+
+    return total
 
 def partB(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    orderingRules, pageNoLists = parse(inputText)
 
-    return 1
+    total = 0
+    for pageNoList in pageNoLists:
+        if not isOrdered(pageNoList, orderingRules):
+            pageNoList = fixOrdering(pageNoList, orderingRules)
+
+            assert isOrdered(pageNoList, orderingRules), "ordering not corrected"
+            total += pageNoList[len(pageNoList) // 2]
+
+    return total
 
 
 
@@ -29,10 +57,18 @@ def readFile(fileName):
         inputText = inputFile.read()
     return inputText
 
-inputText = readFile("einput.txt")
-print("Example partA", partA(inputText))
-print("Example partB", partB(inputText))
-
+inputTextExample = readFile("einput.txt")
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
-#print("partB", partB(inputText))
+
+resultA = partA(inputTextExample)
+print("Example partA", resultA)
+assert resultA == 143
+
+print("partA", partA(inputText))
+
+
+resultB = partB(inputTextExample)
+print("Example partB", resultB)
+assert resultB == 123
+
+print("partB", partB(inputText))
