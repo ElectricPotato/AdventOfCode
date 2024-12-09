246e4240d4742921ffc8db9402f4769b703f2311
ElectricPotato <mironandrzej1@gmail.com>
2024-12-09 01:40:23 +0000

    day 8 part A + B

--- 2024/day08/day08.py
+++ 2024/day08/day08.py
@@ -2,20 +2,79 @@
 
 '''
 
+def parse(inputText):
+    lines=inputText.split("\n")[:-1]
+    width = len(lines[0])
+    height = len(lines)
+
+    antennas = dict()
+
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == '.':
+                continue
+
+            if c in antennas:
+                antennas[c] += [(x, y)]
+            else:
+                antennas[c] = [(x, y)]
+
+    return antennas, width, height
+
 
 def partA(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    antennas, width, height = parse(inputText)
+    antinodes = set()
 
-    return 1
+    for positions in antennas.values():
+        for i in range(len(positions)):
+            for j in range(i):
+                x1, y1 = positions[i]
+                x2, y2 = positions[j]
+                dx, dy = x2 - x1, y2 - y1
+                xNew1, yNew1 = x1 - dx, y1 - dy
+                xNew2, yNew2 = x2 + dx, y2 + dy
+
+                if(0 <= xNew1 < width and 0 <= yNew1 < height):
+                    antinodes.add((xNew1, yNew1))
+
+                if(0 <= xNew2 < width and 0 <= yNew2 < height):
+                    antinodes.add((xNew2, yNew2))
+
+    return len(antinodes)
+
+
+from math import gcd
 
 def partB(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
+    antennas, width, height = parse(inputText)
+    antinodes = set()
 
-    return 1
+    for positions in antennas.values():
+        for i in range(len(positions)):
+            for j in range(i):
+                x1, y1 = positions[i]
+                x2, y2 = positions[j]
+                dx, dy = x2 - x1, y2 - y1
+
+                #reduce fraction
+                g = gcd(dx, dy)
+                dx, dy = dx // g, dy // g
+
+                for n in range(-1000, 1000): #TODO: better limits
+                    xNew, yNew = x1 + n * dx, y1 + n * dy
+
+                    if(0 <= xNew < width and 0 <= yNew < height):
+                        antinodes.add((xNew, yNew))
+
+    #show
+    lines=list(map(list, inputText.split("\n")[:-1]))
+    for x, y in antinodes:
+        lines[y][x] = "#"
+    for line in lines:
+        print(''.join(line))
+
+    return len(antinodes)
 
 
 
@@ -30,9 +89,9 @@ def readFile(fileName):
     return inputText
 
 inputText = readFile("einput.txt")
-print("Example partA", partA(inputText))
-print("Example partB", partB(inputText))
+#print("Example partA", partA(inputText)) #14
+print("Example partB", partB(inputText)) #34
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
-#print("partB", partB(inputText))
+print("partA", partA(inputText)) #320
+print("partB", partB(inputText)) #1157
