47334e6ad356526f69336f7bb3b14366d148cf89
ElectricPotato <mironandrzej1@gmail.com>
2024-12-15 01:57:33 +0000

    day 14 part A

--- 2024/day14/day14.py
+++ 2024/day14/day14.py
@@ -2,13 +2,53 @@
 
 '''
 
+import parse
 
-def partA(inputText):
-    lines=inputText.split("\n")
+def parseRobots(inputText):
+    lines=inputText[:-1].split("\n")
+    
+    parsed = []
     for line in lines:
-        pass
+        lineFormat = "p={:d},{:d} v={:d},{:d}"
+        px, py, vx, vy = parse.parse(lineFormat, line).fixed
+        parsed += [((px, py), (vx, vy))]
 
-    return 1
+    return parsed
+
+def partA(inputText, example = False):
+    robots = parseRobots(inputText)
+
+    if example:
+        width, height = 11, 7
+    else:
+        width, height = 101, 103
+
+    #calulate position of each robot 100 steps in the future
+    futurePositions = []
+    for (px, py), (vx, vy) in robots:
+        for t in range(100):
+            px = (px + vx) % width
+            py = (py + vy) % height
+
+        futurePositions += [(px, py)]
+
+    #count robot in each quadrant
+    quadrants = [0] * 4
+    midX = (width - 1) / 2
+    midY = (height - 1) / 2
+    for x, y in futurePositions:
+        if x > midX:
+            if y > midY:
+                quadrants[0] += 1
+            elif y < midY:
+                quadrants[1] += 1
+        elif x < midX:
+            if y > midY:
+                quadrants[2] += 1
+            elif y < midY:
+                quadrants[3] += 1
+
+    return quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3] 
 
 def partB(inputText):
     lines=inputText.split("\n")
@@ -30,9 +70,9 @@ def readFile(fileName):
     return inputText
 
 inputText = readFile("einput.txt")
-print("Example partA", partA(inputText))
-print("Example partB", partB(inputText))
+print("Example partA", partA(inputText, example=True))
+#print("Example partB", partB(inputText, example=True))
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
+print("partA", partA(inputText))
 #print("partB", partB(inputText))
