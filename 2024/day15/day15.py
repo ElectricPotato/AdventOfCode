ac8896bbfe3e8b5ac0c79ffb0db84cad2aa09238
ElectricPotato <mironandrzej1@gmail.com>
2024-12-15 18:06:48 +0000

    day 15 part A

--- 2024/day15/day15.py
+++ 2024/day15/day15.py
@@ -2,13 +2,91 @@
 
 '''
 
+SQ_OUT_OF_BOUNDS = "x"
+SQ_ROBOT_START = "@"
+SQ_EMPTY = "."
+SQ_WALL = "#"
+SQ_BOX = "O"
+
+dirs = {
+    "^": ( 0, -1), #up
+    ">": (+1,  0), #right
+    "v": ( 0, +1), #down
+    "<": (-1,  0), #left
+}
+
+class Board:
+    def __init__(self, lines) -> None:
+        self.contents = list(map(list, lines.split("\n")))
+
+        self.width = len(self.contents[0])
+        self.height = len(self.contents)
+
+    def getChar(self, x, y):
+        if (0 <= y < self.height) and \
+           (0 <= x < self.width):
+            return self.contents[y][x]
+        return SQ_OUT_OF_BOUNDS
+    
+    def setChar(self, x, y, c):
+        self.contents[y][x] = c
+    
+    #find the first character matching c, scanning left to right, top to bottom
+    def findChar(self, c) -> tuple[int, int]:
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.contents[y][x] == c:
+                    return (x, y)
+                
+    def sumBoxes(self):
+        total = 0
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.contents[y][x] == SQ_BOX:
+                    total += 100 * y + x
+        
+        return total
+
+class Robot:
+    def __init__(self, inputText):
+        lines, self.instructions = inputText.rstrip().split("\n\n")
+        self.instructions = self.instructions.replace("\n", "")
+
+        self.board = Board(lines)
+        self.posX, self.posY = self.board.findChar(SQ_ROBOT_START)
+        self.board.setChar(self.posX, self.posY, SQ_EMPTY)
+
+
+    def move(self, instr):
+        dx, dy = dirs[instr]
+        newX, newY = self.posX + dx, self.posY + dy
+        if self.board.getChar(newX, newY) == SQ_EMPTY:
+            self.posX, self.posY = newX, newY
+        elif self.board.getChar(newX, newY) == SQ_BOX:
+            #raycast in the same direction until an empty space is found
+            scanPosX, scanPosY = newX + dx, newY + dy
+
+            while self.board.getChar(scanPosX, scanPosY) == SQ_BOX:
+                scanPosX, scanPosY = scanPosX + dx, scanPosY + dy
+
+            if self.board.getChar(scanPosX, scanPosY) == SQ_EMPTY:
+                #move box and robot
+                self.board.setChar(scanPosX, scanPosY, SQ_BOX)
+                self.board.setChar(newX, newY, SQ_EMPTY)
+                self.posX, self.posY = newX, newY
+        #elif self.board.getChar(newX, newY) == SQ_WALL:
+        #    pass #do nothing
+        
+    def runAllMoves(self):
+        for instr in self.instructions:
+            self.move(instr)
+
 
 def partA(inputText):
-    lines=inputText.split("\n")
-    for line in lines:
-        pass
-
-    return 1
+    robot = Robot(inputText)
+    robot.runAllMoves()
+    
+    return robot.board.sumBoxes()
 
 def partB(inputText):
     lines=inputText.split("\n")
@@ -31,8 +109,12 @@ def readFile(fileName):
 
 inputText = readFile("einput.txt")
 print("Example partA", partA(inputText))
-print("Example partB", partB(inputText))
+#print("Example partB", partB(inputText))
+
+inputText = readFile("einput2.txt")
+print("Example partA", partA(inputText))
+#print("Example partB", partB(inputText))
 
 inputText = readFile("input.txt")
-#print("partA", partA(inputText))
+print("partA", partA(inputText))
 #print("partB", partB(inputText))
