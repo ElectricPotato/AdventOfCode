af78d5c9741b44ce033df88bcf6c6581bab465da
ElectricPotato <mironandrzej1@gmail.com>
2025-12-05 07:00:13 +0000

    day 05 part A + B + testcases

---
+++ 2025/day05/sol.py
@@ -0,0 +1,85 @@
+import copy
+
+def parseA(filename):
+    with open(filename) as file:
+        fresh_ranges, ingredients = file.read().split("\n\n")
+
+    fresh_ranges = [list(map(int, line.split("-"))) for line in fresh_ranges.split()]
+    ingredients = [int(line) for line in ingredients.split()]
+
+    return (fresh_ranges, ingredients)
+
+def partA(parsed):
+    fresh_ranges, ingredients = parsed
+
+    count = 0
+    for i in ingredients:
+        fresh = False
+        for a, b in fresh_ranges:
+            if a <= i <= b:
+                fresh = True
+                count += 1
+                break
+
+    return count
+
+# boolean union of two ranges a and b
+def range_overlap(a, b):
+    assert a[0] <= a[1] and b[0] <= b[1]
+
+    if range_overlap_check(a, b):
+        return [min(a[0], b[0]), max(a[1], b[1])]
+    else:
+        return None
+    
+# true if ranges (a1, b1) and (a2, b2) overlap or are next to each other
+def range_overlap_check(a, b):
+    return a[0] <= b[0] <= a[1] or a[0] <= b[1] <= a[1] \
+        or abs(a[0] - b[1]) == 1 or abs(a[1] - b[0]) == 1
+    
+def range_size(a):
+    return a[1] - a[0] + 1
+
+def find_overlap(fresh_ranges):
+    for idx1 in range(len(fresh_ranges)):
+        for idx2 in range(idx1 + 1, len(fresh_ranges)):
+            if range_overlap_check(fresh_ranges[idx1], fresh_ranges[idx2]):
+                return (idx1, idx2)
+    return None
+
+def partB(ranges_in):
+    fresh_ranges = copy.deepcopy(ranges_in)
+
+    #union all ranges
+    # this could be made more efficient: https://stackoverflow.com/questions/15273693/union-of-multiple-ranges
+    while True:
+        overlap = find_overlap(fresh_ranges)
+        if overlap == None:
+            break
+
+        idx1, idx2 = overlap
+        fresh_ranges[idx1] = range_overlap(fresh_ranges[idx1], fresh_ranges[idx2])
+        fresh_ranges.pop(idx2)
+
+    return sum(range_size(i) for i in fresh_ranges)
+
+def main():
+    parsedA_example = parseA("einput.txt")
+    result = partA(parsedA_example)
+    print(result)
+    assert result == 3
+
+    parsedA = parseA("input.txt")
+    result = partA(parsedA)
+    print(result)
+
+    result = partB(parsedA_example[0])
+    print(result)
+    assert result == 14
+    
+    result = partB(parsedA[0])
+    print(result)
+    assert result == 347338785050515
+
+if __name__ == '__main__':
+    main()
---
+++ 2025/day05/testcases.py
@@ -0,0 +1,62 @@
+import copy
+from random import randint
+
+from sol import *
+
+#a simple version of the function to use a reference
+def partB_naive(fresh_ranges):
+    ingredients = set()
+    for start, end in fresh_ranges:
+        for i in range(start, end + 1):
+            ingredients.add(i)
+    return len(ingredients)
+
+
+#compare the partB function with the reference one, under random testcases
+def compare():
+    for _ in range(10000):
+        #generate testcase
+        fresh_ranges = []
+        for _ in range(100):
+            start = randint(1, 100)
+            end = start + randint(0, 100)
+            fresh_ranges.append([start, end])
+
+        naive = partB_naive(fresh_ranges)
+        optimised = partB(fresh_ranges)
+
+        assert naive == optimised, f"testcase: {fresh_ranges}\nnaive: {naive}, optimised: {optimised}"
+        print("ok")
+
+
+#remove elements from the testcase to get it down to the source of the discrepency
+def minimise_testcase():
+    #this was a randomly generated tescase that caused the discrepency
+    testcase = [[41, 41], [17, 24], [17, 64], [39, 71], [6, 58], [22, 95], [57, 104], [86, 178], [75, 75], [64, 146], [85, 137], [48, 50], [56, 140], [46, 85], [44, 45], [95, 195], [78, 122], [93, 109], [50, 119], [22, 43], [32, 87], [79, 87], [9, 80], [3, 76], [93, 156], [34, 101], [80, 175], [32, 100], [17, 60], [39, 67], [37, 128], [14, 99], [3, 79], [98, 169], [9, 70], [68, 69], [22, 107], [58, 96], [46, 74], [14, 27], [1, 54], [5, 65], [67, 102], [57, 97], [87, 181], [50, 70], [81, 87], [85, 143], [24, 71], [15, 100], [79, 176], [53, 137], [15, 15], [61, 76], [43, 60], [45, 75], [74, 117], [52, 137], [83, 182], [60, 71], [44, 106], [94, 145], [88, 137], [39, 113], [60, 157], [33, 83], [13, 35], [9, 22], [78, 148], [5, 48], [32, 121], [76, 78], [34, 77], [58, 135], [58, 105], [67, 70], [40, 40], [28, 88], [3, 9], [83, 117], [7, 94], [6, 59], [56, 129], [71, 140], [70, 128], [74, 172], [18, 59], [81, 83], [38, 69], [1, 96], [36, 116], [9, 18], [77, 110], [88, 125], [84, 169], [87, 144], [82, 108], [53, 143], [87, 140], [4, 49]]
+    #verify there is in fact a discrepency
+    assert partB_naive(testcase) != partB(testcase), "there is no discrepency, cant minimise"
+
+    while True:
+        found = False
+        for i in range(len(testcase)):
+            new_testcase = copy.deepcopy(testcase)
+            new_testcase.pop(i)
+
+            naive = partB_naive(new_testcase)
+            optimised = partB(new_testcase)
+
+            if naive != optimised:
+                testcase = copy.deepcopy(new_testcase)
+                found = True
+                break
+
+        if not found:
+            break
+
+    
+    print("minimused testcase: ", testcase)
+
+#compare()
+minimise_testcase()
+assert partB_naive([[82, 108], [53, 143]]) == partB([[82, 108], [53, 143]]), "there is a discrepency"
+assert range_overlap_check([82, 108], [53, 143])
