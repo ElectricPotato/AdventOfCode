9e26e14f91cd9f6006ab1dc6c80ef665f195c972
ElectricPotato <mironandrzej1@gmail.com>
2025-12-05 03:44:06 +0000

    day 03 part B

--- 2025/day03/sol.py
+++ 2025/day03/sol.py
@@ -20,9 +20,22 @@ def partA_one_line(line):
 def partA(parsed):
     return sum(partA_one_line(line) for line in parsed)
 
-def partB(parsed):
+def partB_one_line(line):
+    total_digits = 12
 
-    return None
+    result = 0
+    for digit_idx in range(total_digits):
+        remaining_digits = total_digits - digit_idx
+
+        current_digit = max(line[:len(line) - remaining_digits + 1])
+        line = line[line.index(current_digit) + 1:]
+
+        result = result * 10 + current_digit #append new digit to result
+
+    return result
+
+def partB(parsed):
+    return sum(partB_one_line(line) for line in parsed)
 
 def main():
     parsedA_example = parseA("einput.txt")
@@ -35,15 +48,18 @@ def main():
     print(result)
     assert result == 17087
 
+    assert partB_one_line(list(map(int,"987654321111111"))) == 987654321111
+    assert partB_one_line(list(map(int,"811111111111119"))) == 811111111119
+    assert partB_one_line(list(map(int,"234234234234278"))) == 434234234278
+    assert partB_one_line(list(map(int,"818181911112111"))) == 888911112111
+
     result = partB(parsedA_example)
     print(result)
-    assert result == None
+    assert result == 3121910778619
     
     result = partB(parsedA)
     print(result)
-    #assert result ==
-
-    
+    assert result == 169019504359949
 
 
 if __name__ == '__main__':
