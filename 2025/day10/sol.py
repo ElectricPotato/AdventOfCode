81fcb7c8ce560b184186f0024a8d61f6d487953d
ElectricPotato <mironandrzej1@gmail.com>
2025-12-08 06:07:18 +0000

    add templates for days 9 - 12

---
+++ 2025/day10/sol.py
@@ -0,0 +1,40 @@
+def parseA(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+
+    lines = [line.replace("\n","") for line in lines]
+
+    return lines
+
+def partA(parsed):
+
+    return None
+
+def partB(parsed):
+
+    return None
+
+def main():
+    parsedA_example = parseA("einput.txt")
+    result = partA(parsedA_example)
+    print(result)
+    assert result == None
+
+    parsedA = parseA("input.txt")
+    result = partA(parsedA)
+    print(result)
+    #assert result == 
+
+    result = partB(parsedA_example)
+    print(result)
+    assert result == None
+    
+    result = partB(parsedA)
+    print(result)
+    #assert result == 
+
+    
+
+
+if __name__ == '__main__':
+    main()
