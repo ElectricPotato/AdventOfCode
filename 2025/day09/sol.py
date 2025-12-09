74dbc4550a8688c0118d9fab29b04d5287099eb9
ElectricPotato <mironandrzej1@gmail.com>
2025-12-09 12:15:12 +0000

    day 09 part A

--- 2025/day09/sol.py
+++ 2025/day09/sol.py
@@ -2,15 +2,26 @@ def parseA(filename):
     with open(filename) as file:
         lines = file.readlines()
 
-    lines = [line.replace("\n","") for line in lines]
+    lines = [list(map(int, line.replace("\n","").split(","))) for line in lines]
 
     return lines
 
-def partA(parsed):
+def area(a, b):
+    return abs(a[0] - b[0] + 1) * abs(a[1] - b[1] + 1)
 
-    return None
+def partA(squares):
+    rectangles = []
+    for i in range(len(squares)):
+        for j in range(i + 1, len(squares)):
+            rectangles.append((i, j, area(squares[i], squares[j])))
 
-def partB(parsed):
+    rectangles.sort(key=lambda x: x[2], reverse=True)
+
+    return rectangles[0][2]
+
+def partB(points):
+
+    polygon_lines = list(zip(points, points[1:]))
 
     return None
 
@@ -18,16 +29,16 @@ def main():
     parsedA_example = parseA("einput.txt")
     result = partA(parsedA_example)
     print(result)
-    assert result == None
+    assert result == 50
 
     parsedA = parseA("input.txt")
     result = partA(parsedA)
     print(result)
-    #assert result == 
+    assert result == 4774877510
 
     result = partB(parsedA_example)
     print(result)
-    assert result == None
+    assert result == 24
     
     result = partB(parsedA)
     print(result)
