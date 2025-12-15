7c445f22144d8f75f881c0ec66f9f2a44b9abaad
ElectricPotato <mironandrzej1@gmail.com>
2025-12-15 18:57:05 +0000

    day 12 part A parsing

--- 2025/day12/sol.py
+++ 2025/day12/sol.py
@@ -4,9 +4,32 @@ def parseA(filename):
 
     lines = [line.replace("\n","") for line in lines]
 
-    return lines
+    shapes = []
+
+    while lines[0][-1] == ":":
+        lines.pop(0) #remove the shape numbering
+
+        shape = []
+        while "#" in lines[0] or "." in lines[0]:
+            shape += [[c == "#" for c in lines[0]]]
+            lines.pop(0)
+
+        lines.pop(0) #remove the blank line
+
+        shapes.append(shape)
+
+    cases = []
+    for line in lines:
+        dimentions, shape_numbers = line.split(": ")
+        dimentions = list(map(int, dimentions.split("x")))
+        shape_numbers = list(map(int, shape_numbers.split(" ")))
+
+        cases.append((dimentions, shape_numbers))
+
+    return (shapes, cases)
 
 def partA(parsed):
+    print(parsed)
 
     return None
 
@@ -18,7 +41,7 @@ def main():
     parsedA_example = parseA("einput.txt")
     result = partA(parsedA_example)
     print(result)
-    assert result == None
+    assert result == 2
 
     parsedA = parseA("input.txt")
     result = partA(parsedA)
