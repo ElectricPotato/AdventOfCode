d6f5371ded8e400fdd37077c293c74701be80a06
ElectricPotato <mironandrzej1@gmail.com>
2025-12-16 04:52:23 +0000

    day 10 part B - linear algebra development

--- 2025/day10/sol.py
+++ 2025/day10/sol.py
@@ -1,3 +1,5 @@
+from typing import List
+
 def parseA(filename):
     with open(filename) as file:
         lines = file.readlines()
@@ -52,37 +54,151 @@ def partA_one(machine):
 def partA(parsed):
     return sum(partA_one(machine) for machine in parsed)
 
-#recursive solution
-def partA_one(buttons, joltage):
-    if len(joltage) > 1:
-        pass #recurse
-    else:
-        valid_buttons_values = [button[0][0] for button in buttons if joltage[0] % button[0][0] == 0]
-        if len(valid_buttons_values) == 0:
-            return None
+def new_matrix(n_rows, n_cols):
+    return [[0] * n_cols for _ in range(n_rows)]
+
+def row_leading_zeros(row):
+    count = 0
+    for element in row:
+        if element != 0:
+            break
+        count += 1
+
+    return count
+
+#row_a -= row_b * n
+def sub_row(m, row_a, row_b, n):
+    for i in range(len(m[0])):
+        m[row_a][i] -= m[row_b][i] * n
+
+#row_i *= n
+def divide_row(m, row_i, n):
+    for i in range(len(m[0])):
+        #assert m[row_i][i] % n == 0, "a non-integer division has happened"
+        #if(m[row_i][i] % n != 0): print(f"non integer {m[row_i][i]}/{n}")
+        m[row_i][i] /= n
+
+#turn into Row Echelon Form
+def matrix_ref(m: List[List[int]]):
+
+    row_n = 0
+    col_n = 0
+    m.sort(key = row_leading_zeros)
+
+    while True:
+        divide_row(m, row_n, m[row_n][col_n]) #normalise row
+        for row_idx in range(row_n + 1, len(m)):
+            sub_row(m, row_idx, row_n, m[row_idx][col_n])
+
+        m.sort(key = row_leading_zeros) #sort after row operations
+
+        #find the next pivot (non zero value)
+        row_n += 1
+        col_n += 1
+        if row_n >= len(m) or col_n >= len(m[0]) - 1:
+            break
+
+        while m[row_n][col_n] == 0:
+            col_n += 1
+
+            if col_n >= len(m[0]) - 1:
+                break
+
+        if col_n >= len(m[0]) - 1:
+                break
+
+#turn into Row Echelon Form
+#(reduce as much as possible)
+def matrix_rref(m: List[List[int]]):
+    row_n = 0
+    col_n = 0
+
+    while True:
+        for row_i in range(len(m)):
+            if row_i == row_n:
+                continue
+            sub_row(m, row_i, row_n, m[row_i][col_n])
+
+        #find the next pivot (non zero value)
+        row_n += 1
+        col_n += 1
+        if row_n >= len(m) or col_n >= len(m[0]) - 1:
+            break
+
+        while m[row_n][col_n] == 0:
+            col_n += 1
+
+            if col_n >= len(m[0]) - 1:
+                break
+
+        if col_n >= len(m[0]) - 1:
+                break
+
+def remove_zero_rows(m):
+    row_idx = 0
+    while row_idx < len(m):
+        if all(e == 0 for e in m[row_idx]):
+            m.pop(row_idx)
         else:
-            return joltage[0] // max(valid_buttons_values)
+            row_idx += 1
+
+
+def show_matrix(m):
+    for row in m:
+        print('\t'.join(list(map(str, row))))
+
+#Linear algebra solution
+def partA_one(buttons, joltages):
+
+    #augmented matrix
+    button_matrix = new_matrix(len(joltages), len(buttons) + 1)
+
+    #populate matrix
+    for button_idx, button in enumerate(buttons):
+        for joltage_idx in button:
+            button_matrix[joltage_idx][button_idx] = 1
+
+    for joltage_idx, joltage in enumerate(joltages):
+        button_matrix[joltage_idx][len(buttons)] = joltage
+
+    #turn into Row Echelon Form
+    matrix_ref(button_matrix)
+
+    #turn into (somewhat) Reduced Row Echelon Form
+    matrix_rref(button_matrix)
+
+    remove_zero_rows(button_matrix)
+
+    #print()
+    #show_matrix(button_matrix)
+    free_var = len(button_matrix[0]) - 1 - len(button_matrix)
+    print("free variables", )
+    if free_var <= 0:
+        show_matrix(button_matrix)
+    print()
+
+    return 0
 
 def partB(parsed):
     return sum(partA_one(buttons, joltage) for _, buttons, joltage in parsed)
 
 def main():
     parsedA_example = parseA("einput.txt")
-    result = partA(parsedA_example)
-    print(result)
-    assert result == 7
+    #result = partA(parsedA_example)
+    #print(result)
+    #assert result == 7
 
     parsedA = parseA("input.txt")
-    result = partA(parsedA)
-    print(result)
-    assert result == 436
+    #result = partA(parsedA)
+    #print(result)
+    #assert result == 436
 
     result = partB(parsedA_example)
-    print(result)
-    assert result == 33
+    #print(result)
+    #assert result == 33
     
     result = partB(parsedA)
-    print(result)
+    #print(result)
     #assert result == 
 
     
